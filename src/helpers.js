e5c6a1eee96c2e39e16913257d7660fcebfe498e
viktorija <viktorija.kotaite@gmail.com>
2024-01-24 12:31:01 +0200

    helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,26 +1,24 @@
-const mysql = require('mysql2/promise');
-const { dbConfig } = require('./config');
+const mysql = require("mysql2/promise");
+const { dbConfig } = require("./config");
 
+async function executeQuery(sql, arguments = []) {
+  let connection;
 
-async function executeQuery(sql, arguments = []){
-    let connection;
-
-    try {
-        connection = await mysql.createConnection(dbConfig)
+  try {
+    connection = await mysql.createConnection(dbConfig);
 
     const [rows] = await connection.execute(sql, arguments);
 
-    return [rows, null]
-    } catch (error) {
-        console.log('error in connection mysql ===', error);
-        return [null, error]
-    } finally {
-        if(connection){
-            connection.end()
-        }
+    return [rows, null];
+  } catch (error) {
+    console.log("error in connection mysql ===", error);
+    return [null, error];
+  } finally {
+    if (connection) {
+      connection.end();
     }
-    
+  }
 }
 module.exports = {
-    executeQuery
-}
+  executeQuery,
+};
