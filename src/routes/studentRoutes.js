51dcf0efe87b8defd4e6665ac48fb0bca4850ef9
viktorija <viktorija.kotaite@gmail.com>
2024-01-25 10:35:39 +0200

    student routes

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -4,5 +4,9 @@ const studentController = require('../controllers/studentController');
 const studentRouter = express.Router();
 
 studentRouter.get('/students', studentController.all);
+studentRouter.get('/students/:studentId', studentController.single);
+studentRouter.post('/students', studentController.create);
+studentRouter.put('/students/:studentId', studentController.update);
+studentRouter.delete('/students/:studentId', studentController.delete);
 
 module.exports = studentRouter;
