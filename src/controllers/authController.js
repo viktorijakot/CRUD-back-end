8f156ced28cb6d2a0139c5e8f40952345dad9cd9
viktorija <viktorija.kotaite@gmail.com>
2024-01-30 11:50:50 +0200

    register controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,26 @@
+const bcrypt = require('bcrypt');
+const { executeQuery } = require('../helpers');
+
 const register = async (req, res, next) => {
   const { email, password } = req.body;
 
-  console.log('req.body ===', req.body);
+  const passwordHash = bcrypt.hashSync(password, 10);
+
+  const sql = 'INSERT INTO user (email, password, scope) VALUES (?,?, "manager")';
+
+  const [respObj, error] = await executeQuery(sql, [email, passwordHash]);
+
+  if (error) {
+    console.log('error register user ===');
+    next(error);
+  }
+  if (respObj.affectedRows === 1) {
+    res.status(201).json({
+      msg: 'User is created',
+      id: respObj.insertId,
+    });
+  }
+  res.end();
 };
 
 module.exports = {
